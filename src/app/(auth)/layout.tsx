3b2af5bfeeb531293adeef7be980eba20cc93754
Oluwatimilehin Akinsanmi <akinsanmioluwatimilehin@gmail.com>
2026-05-18 21:09:16 +0100

    Use environment variable for Google Client ID

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { RedirectIfAuthed } from "@/features/auth/components/RedirectIfAuthed";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  const clientId = "686601436024-nob7de7vekj0pnflm5uig3ehj1l9uipk.apps.googleusercontent.com";
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
   if (!clientId) console.warn("Google Client ID is missing");
 
   return (
     <GoogleOAuthProvider clientId={clientId || ""}>
+      <RedirectIfAuthed />
       {children}
     </GoogleOAuthProvider>
   );
