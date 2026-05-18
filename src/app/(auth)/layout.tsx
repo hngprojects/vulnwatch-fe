57f7fe6f2a2ec944b374a970c045e102d2248694
Oluwatimilehin Akinsanmi <akinsanmioluwatimilehin@gmail.com>
2026-05-18 15:19:16 +0100

    client code

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const clientId = "686601436024-nob7de7vekj0pnflm5uig3ehj1l9uipk.apps.googleusercontent.com";
   if (!clientId) console.warn("Google Client ID is missing");
 
   return (
